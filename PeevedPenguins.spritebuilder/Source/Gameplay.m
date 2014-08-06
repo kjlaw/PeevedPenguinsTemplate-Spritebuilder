5b852b58f634ac443f9300ef6e414ed32577c5ad
Kristen <kristenlaw.2014@gmail.com>
2014-08-06 17:49:34 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -135,4 +135,9 @@
     [self releaseCatapult];
 }
 
+-(void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair seal:(CCNode *)nodeA wildcard:(CCNode *)nodeB
+{
+    CCLOG(@"Something collided with a seal!");
+}
+
 @end
